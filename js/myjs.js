7a31808ce44e2d4e5fd0a0e2568e3a568d137b82
timecity08864402 <34847481+timecity08864402@users.noreply.github.com>
2018-04-20 18:41:27 +0800

    Add files via upload

--- js/myjs.js
+++ js/myjs.js
@@ -3,5 +3,7 @@ $(document).ready(function() {
         $(".pay_add").toggleClass("pay_add_active");
         $(".onlineMenu>ul").toggleClass("onlineMenu_active");
     });
-
+    $(".moneybox li").click(function () {
+        $(this).addClass("moneybox_active").siblings().removeClass("moneybox_active");
+    });
 });
